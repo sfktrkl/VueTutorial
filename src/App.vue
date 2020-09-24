cbcc6ee5f9ae04112bbcf048b8970e48c44f885b
Safak TURKELI <sfktrkl@gmail.com>
2020-09-25 00:47:27 +0300

    Vue Files & The Root Component

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,40 @@
+<!-- .vue file is just an extension of a vue component. -->
+<!-- This component has a template like other components -->
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>{{title}}</h1>
+
+    <!-- Vue Files & The Root Component -->
+    <h2>Vue Files & The Root Component</h2>
+    <p>{{greeting()}}</p>
   </div>
 </template>
 
+
+<!-- Inside this html, data can be accessed by exporting an object inside the script tag -->
 <script>
-export default {
+  export default {
+  // Name of the component
   name: 'app',
+  // We are returning the data like other compoenents
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      // Vue Files & The Root Component
+      title: 'Vue Tutorial',
     }
-  }
+    },
+    methods: {
+      // Vue Files & The Root Component
+      greeting: function () {
+        return 'Hello from the root component';
+      },
+    },
+    computed: {
+
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// We have root vue instance which is controlling the #app.
+// We are rendering the root component App.vue which is
+// imported above to #app element.
 new Vue({
   el: '#app',
   render: h => h(App)
